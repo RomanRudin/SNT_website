750a8afa6461f4e0edd26c5b2ac6db18c32fae1f
Roman <rudin.roman@bk.ru>
2024-07-20 12:37:58 +0200

    Work is in progress

---
+++ galery.js
@@ -0,0 +1,41 @@
+let left = document.getElementById("arrowleft");
+let right = document.getElementById("arrowright");
+let carousel = document.getElementById("carousel");
+
+for (let i = 1; i <= 13; i++){
+    let img = document.createElement("img");
+    img.src = "src/image" + i + ".jpg"
+    carousel.appendChild(img);
+}
+
+left.addEventListener('click', clickLeft);
+right.addEventListener('click', clickRight);
+
+const carouselElem = 2;
+const width = 300 / carouselElem;
+const item = carousel.children.length - carouselElem;
+let current = 0;
+
+for (let i=0; i < carousel.childElementCount; i++){
+    carousel.children[i].style.width = width + '%';
+    carousel.children[i].style.maxWidth = width + '%';
+}
+
+function clickLeft(){
+    current--;
+    if (current >= 0){
+        carousel.style.transform = `translateX(-${width * current}%)`;
+    }
+    else{
+        current = 0;
+    }
+}
+function clickRight(){
+    current++;
+    if (current <= item){
+        carousel.style.transform = `translateX(-${width * current}%)`;
+    }
+    else{
+        current = item;
+    }
+}
---
+++ script.js
@@ -0,0 +1,12 @@
+function Randomizer(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function mainImageChooser(){
+    const main = document.getElementsByTagName('main')[0];
+    if (main.id == "galery-main") return;
+    main.style.background = "url(src/image" + Randomizer(1, 13) + ".jpg) no-repeat center";
+    main.style.backgroundSize = "cover";
+}
+
+mainImageChooser();
